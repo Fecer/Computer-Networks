79b9456898e5add029a2dad627cfaa318585438d
zack-guo <52080300+zack-guo@users.noreply.github.com>
2020-04-21 22:01:08 +0800

    Create frame.py

---
+++ 3.3/frame.py
@@ -0,0 +1,14 @@
+class Frame:
+    id=0
+    def __init__(self, buffer, seq):
+        frame.info = buffer
+        frame.seq == next_frame_to_send
+    def crc(self):
+        
+
+class packet(object):
+    def __init__(self):
+        print(self)
+    def generate(self):
+    def transparency(self):
+
